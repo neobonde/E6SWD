920ba2e6f81c0d8b1bbd108fb680c6db0e55446f
Nicolai Bonde <201404519@post.au.dk>
2017-08-27 12:18:40 +0200

    Completeted week 00 exercise 2

---
+++ Week00/Interfaces-2/Interfaces-2/Vehicles.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_2
+{
+    class Vehicles
+    {
+        public class GasEngine : IEngine
+        {
+            private uint _curThrottle = 0;
+            private uint _maxThrottle = 0;
+            public GasEngine(uint maxThrottle)
+            {
+                _maxThrottle = maxThrottle;
+            }
+            public uint MaxThrottle
+            {
+                get { return _maxThrottle; }
+            }
+            public void SetThrottle(uint thr)
+            {
+                _curThrottle = thr;
+            }
+            public uint GetThrottle()
+            {
+                return _curThrottle;
+            }
+        }
+
+        public class MotorBike
+        {
+            private IEngine _engine = null;
+            public MotorBike(IEngine engine)
+            {
+                _engine = engine;
+            }
+            void RunAtHalfSpeed()
+            {
+                _engine.SetThrottle(_engine.MaxThrottle / 2);
+            }
+        }
+        public class Application
+        {
+            static void Main(string[] args)
+            {
+                IEngine engine = new GasEngine(2);
+                MotorBike myBike = new MotorBike(engine);
+            }
+        }
+
+    }
+}
